75c152f6344b7d4d673e472c44d998f752e63090
Kyle Mathenia <66032082+kylemathenia@users.noreply.github.com>
2022-01-13 20:48:03 -0600

    Update cal_steering.py

--- lib/cal_steering.py
+++ lib/cal_steering.py
@@ -7,20 +7,20 @@ def cal_steering(px):
     desired_angle = 0
     while True:
         input("\nMove the robot to an open area. \nPress enter to begin calibration.\n> ")
-        angle_calibrated = desired_angle + px.dir_cal_value
-        px.servo_dir.angle(angle_calibrated)
         px.forward(50)
         time.sleep(2)
         px.stop()
         while True:
             try:
-                error = float(input("How many degrees of angle was the steering off? "
-                                    "\n(Negative values for left, positive values for right, zero if approx straight)\n> "))
+                error = int(float(input("How many degrees of angle was the steering off? "
+                                    "\n(Negative values for left, positive values for right, zero if approx straight)\n> ")))
                 break
             except:
                 print("\nInvalid entry. Please enter a number.")
         if error == 0: break
         px.dir_cal_value = px.dir_cal_value + error
+        angle_calibrated = desired_angle + px.dir_cal_value
+        px.servo_dir.angle(angle_calibrated)
     px.config_file_obj.set("picarx_dir_servo", "%s" % px.dir_cal_value)
 
 if __name__ == '__main__':
