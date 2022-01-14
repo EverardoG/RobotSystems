17f1da62c497893de19ce675f29209a361924aaa
Kyle Mathenia <66032082+kylemathenia@users.noreply.github.com>
2022-01-13 20:58:55 -0600

    Update test_simple_moves.py

--- lib/test_simple_moves.py
+++ lib/test_simple_moves.py
@@ -1,5 +1,6 @@
 import picarx_improved
 import logging
+import time
 
 
 if __name__ == '__main__':
@@ -7,7 +8,11 @@ if __name__ == '__main__':
     px = picarx_improved.Picarx()
     input('\nPress enter to go forward: ')
     px.forward(50)
+    time.sleep(2)
+    px.stop()
     input('\nPress enter to change the steering angle: ')
     px.set_dir_servo_angle(10)
     input('\nPress enter to go forward: ')
     px.forward(50)
+    time.sleep(2)
+    px.stop()
