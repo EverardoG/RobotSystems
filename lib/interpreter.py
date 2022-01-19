ea518c3f9b067f0adf271c57c9dbcd032b71cfbe
Kyle Mathenia <66032082+kylemathenia@users.noreply.github.com>
2022-01-19 01:48:59 -0600

    x

--- lib/interpreter.py
+++ lib/interpreter.py
@@ -4,27 +4,71 @@
 import picarx_improved
 import logging
 import time
-
+import numpy as np
 
 class Interpreter(object):
     """Class for interpreting the grayscale sensor data into a discrete position."""
-    def __init__(self, sensitivity = 50, line_polarity='darker'):
-        self.sensitivity = sensitivity
-        self.line_polarity = line_polarity  # 'darker' if line is darker than surroundings. 'lighter' if opposite.
-        self.previous_sensor_data = None
 
-    def get_position(self,sensor_data):
+    def __init__(self, sensitivity=50, line_polarity='darker'):
+        polarity_map = {'darker':1,'lighter':-1}
+        self.line_polarity = polarity_map[line_polarity]
+        self.sensitivity = sensitivity / 50000
+        self.running_data = [ [], [], [] ]
+        self.running_aves = [0,0,0]
+        self.changes = [0,0,0]
+        self.moving_ave_num = 5
+        self.buffer_full = False
+
+    def get_position(self, sensor_data):
         """Interprets the sensor data and returns the discrete position of the robot. sensor_data argument is a list
         of length 3. Returns -1, 0, or 1 for if the robot is to the left, center, or right of the line."""
+        if self.buffer_full:
+            for i in range(len(sensor_data)):
+                self.running_data[i].append(sensor_data[i])
+                del self.running_data[i][0]
+                ave = np.average(self.running_data[i])
+                self.changes[i] = (ave - self.running_aves) * self.moving_ave_num
+        else:  # Buffer isn't full yet.
+            buffer_size = self._add_to_buffer(sensor_data)
+            if buffer_size == self.moving_ave_num: self.buffer_full = True
+            return 0  # Return a neutral position until buffer fills.
+
+        # Combine the average value and
+        direction = (self.running_aves + self.changes[i]) * self.sensitivity
+        direction -= np.min(direction)  # adjust down so the lowest value is zero.
+        # 'Vote' on which direction to go.
+        direction[0] = direction[0] * -1
+        direction[1] = 0
+        direction[2] = direction[2] * 1
+        direction = np.sum(direction) * self.line_polarity
+        if direction < -1: direction = -1
+        if direction > 1: direction = 1
+        return direction
 
 
+    def _add_to_buffer(self, sensor_data):
+        for i in range(len(sensor_data)):
+            self.running_data[i].append(sensor_data[i])
+            ave = np.average(self.running_data[i])
+            self.changes[i] = ave - self.running_aves
+            self.running_aves[i] = ave
+        buffer_size = len(self.running_data[0])
+        return buffer_size
 
-        self.previous_sensor_data = sensor_data
-        pass
+
+def test():
+    data = [[191, 223, 210],[181, 230, 214],[185, 224, 207],[184, 225, 211],[187, 224, 211],[186, 233, 205],
+            [181, 232, 206],[190, 226, 210],[187, 226, 211],[182, 229, 213],[184, 229, 211],[185, 231, 210],
+            [190, 227, 207],[187, 230, 210],[185, 227, 210]]
+    interpreter = Interpreter()
+    for i in range(len(data)):
+        print(interpreter.get_position(data[i]))
+
+
 
 def main():
     logging.getLogger().setLevel(logging.INFO)
-    pass
+    test()
 
 if __name__ == '__main__':
     main()
