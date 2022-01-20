22124d9744e376218519c855a6ae705864acb6c8
Kyle Mathenia <66032082+kylemathenia@users.noreply.github.com>
2022-01-19 23:25:52 -0600

    Update line_following_controller.py

--- lib/line_following_controller.py
+++ lib/line_following_controller.py
@@ -21,7 +21,7 @@ class Controller(object):
         self.pwm_percent = pwm_percent
         self.dir_range = [-1, 1]
         self.steering_angle_range = [-90, 90]
-        self.move_ave_num = 10
+        self.move_ave_num = 1
         self.dir_vals = [0]*self.move_ave_num
         atexit.register(self.shutdown)
 
